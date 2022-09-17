610df03cdb24a7337608e66f40c4a39378f5206c
SHEKU J SESAY <jeremiahsheku2000@gmail.com>
2022-09-17 02:47:49 +0000

    Explore some array

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+const myDistrict = ['falaba', 'bombali', 'western urban', 'pujehun', 'bo', 'moyamba', 'tonkolli', ' bonthe', 'kono', 'koinadugu', 'kailahun', 'karena', 'western rural'];
+
+console.log(myDistrict.length);
+console.log(myDistrict.join(",   "));
+
+// template strings
+console.log(`I went to ${myDistrict[5]} 7 months ago`);
+
+
+
+
